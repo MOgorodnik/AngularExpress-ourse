512c125a8397d9c0423147ff3705b1148b83a645
MOgorodnik <mr.ogorodnik@gmail.com>
2020-08-29 10:34:56 +0300

    Second lesson complited. usersList added and generate using *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularExpress-ourse';
+  title = 'AngularExpress-course';
+  appName = 'Додаток "Список користувачів" працюэ.';
+  // typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
+
+// import {Component} from '@angular/core';
+
+/**
+ * @title List with selection
+ */
+// @Component({
+//   selector: 'list-selection-example',
+//   styleUrls: ['list-selection-example.css'],
+//   templateUrl: 'list-selection-example.html',
+// })
+// export class ListSelectionExample {
+//   typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
